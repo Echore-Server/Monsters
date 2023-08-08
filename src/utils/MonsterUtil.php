d09a3e4f30bc94c135f22b8325bc50f6ceb8fdb8
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-08-08 20:54:15 +0900

    `MonsterUtil`のフィルター系メソッドの引数を`Traversable`に変更

--- src/utils/MonsterUtil.php
+++ src/utils/MonsterUtil.php
@@ -7,10 +7,9 @@ namespace Lyrica0954\Monsters\utils;
 use Generator;
 use Lyrica0954\Monsters\entity\MonsterBase;
 use Lyrica0954\Monsters\entity\player\MonsterPlayer;
-use Lyrica0954\SmartEntity\entity\walking\FightingEntity;
 use pocketmine\entity\Entity;
-use pocketmine\entity\Living;
 use pocketmine\player\Player;
+use Traversable;
 
 class MonsterUtil {
 
@@ -26,28 +25,13 @@ class MonsterUtil {
 		return ($entity instanceof Player) ? MonsterPlayer::get($entity) : $entity;
 	}
 
-	/**
-	 * @param Entity $entity
-	 *
-	 * @return Living|null
-	 */
-	public static function castFighting(Entity $entity): ?Living {
-		$casted = self::cast($entity, false);
-
-		if ($casted->getEntity() instanceof FightingEntity) {
-			return $casted->getEntity();
-		}
-
-		return null;
-	}
-
 
 	/**
-	 * @param Generator|Entity[]|RayTraceEntityResult[] $ite
+	 * @param Traversable<mixed, (Entity|RayTraceEntityResult)> $ite
 	 *
 	 * @return Generator<mixed, mixed, MonsterBase, mixed>
 	 */
-	public static function filterMonster(array|Generator $ite, bool $player = false): Generator {
+	public static function filterMonster(Traversable $ite, bool $player = false): Generator {
 		foreach ($ite as $entity) {
 			if ($entity instanceof RayTraceEntityResult) {
 				$entity = $entity->getEntity();
@@ -61,32 +45,29 @@ class MonsterUtil {
 	}
 
 	/**
-	 * @param Generator|RayTraceEntityResult[] $ite
+	 * @param Traversable<mixed, RayTraceEntityResult> $ite
 	 * @param bool $player
 	 *
-	 * @return Generator<mixed, mixed, array{0: RayTraceEntityResult, 1: MonsterBase}, mixed>
+	 * @return Generator<mixed, RayTraceEntityResult, MonsterBase, mixed>
 	 */
-	public static function filterMonsterRayTrace(Generator $ite, bool $player = false): Generator {
+	public static function filterMonsterRayTrace(Traversable $ite, bool $player = false): Generator {
 		foreach ($ite as $result) {
 			$entity = $result->getEntity();
 
 			$e = self::cast($entity, $player);
 
 			if ($e instanceof MonsterBase) {
-				yield [
-					0 => $result,
-					1 => $e
-				];
+				yield $result => $e;
 			}
 		}
 	}
 
 	/**
-	 * @param Generator|Entity[] $ite
+	 * @param Traversable<mixed, Entity> $ite
 	 *
 	 * @return Generator<mixed, mixed, (MonsterBase&FightingEntity), mixed>
 	 */
-	public static function filterFightMonster(Generator $ite, bool $includePlayer = false): Generator {
+	public static function filterFightMonster(Traversable $ite, bool $includePlayer = false): Generator {
 		foreach ($ite as $entity) {
 			$e = self::cast($entity, false);
 
