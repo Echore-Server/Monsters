3402936c14988931b147d5b960fc7f8bd0cbbc40
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-09-26 00:41:00 +0900

    `DistanceAlgo`のTimings計測を削除

--- src/search/algo/distance/DistanceAlgo.php
+++ src/search/algo/distance/DistanceAlgo.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 
 namespace Lyrica0954\Monsters\search\algo\distance;
 
-use Lyrica0954\Monsters\timings\MonstersTimings;
 use Lyrica0954\Monsters\utils\Utils;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Vector3;
@@ -13,27 +12,17 @@ use pocketmine\math\Vector3;
 abstract class DistanceAlgo {
 
 	public function distanceBoundingBox(AxisAlignedBB $bb, Vector3 $pos): float {
-		MonstersTimings::$boundingBoxDistance->startTiming();
 		$point = Utils::getNearestPoint($bb, $pos);
 
-		$dist = $this->distance($point, $pos);
-
-		MonstersTimings::$boundingBoxDistance->stopTiming();
-
-		return $dist;
+		return $this->distance($point, $pos);
 	}
 
 	abstract public function distance(Vector3 $a, Vector3 $b): float;
 
 	public function distanceSquaredBoundingBoxIfSupported(AxisAlignedBB $bb, Vector3 $pos): float {
-		MonstersTimings::$boundingBoxDistance->startTiming();
 		$point = Utils::getNearestPoint($bb, $pos);
 
-		$dist = $this->distanceSquaredIfSupported($point, $pos);
-
-		MonstersTimings::$boundingBoxDistance->stopTiming();
-
-		return $dist;
+		return $this->distanceSquaredIfSupported($point, $pos);
 	}
 
 	abstract public function distanceSquaredIfSupported(Vector3 $a, Vector3 $b): float;
